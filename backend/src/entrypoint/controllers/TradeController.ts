b165a34669b35cfdb4ad5ba4560ed04c8318b558
Adam Cole <adamcole123@hotmail.co.uk>
2022-04-26 20:40:39 +0100

    Sell stocks use case
    Sell stocks use case working and api endpoint built

--- backend/src/entrypoint/controllers/TradeController.ts
+++ backend/src/entrypoint/controllers/TradeController.ts
@@ -6,15 +6,18 @@ import dotenv from 'dotenv';
 import ITradeDto from '../../usecases/data_tranfer_objects/ITradeDto';
 import IBuyStocksUseCase from '../../usecases/Trades/IBuyStocksUseCase';
 import TradeServiceLocator from "../../configuration/TradeServiceLocator";
+import ISellStocksUseCase from "../../usecases/Trades/ISellStocksUseCase";
 
 dotenv.config();
 
 @controller('/trade')
 export default class TradeController implements interfaces.Controller {
 	private readonly buyStocksUseCase: IBuyStocksUseCase;
+	private readonly sellStocksUseCase: ISellStocksUseCase;
 	
 	constructor(@inject(TYPES.TradeServiceLocator) serviceLocator: TradeServiceLocator){
 		this.buyStocksUseCase = serviceLocator.GetBuyStocksUseCase();
+		this.sellStocksUseCase = serviceLocator.GetSellStocksUseCase();
 	}
 	
 	@httpPost('/buystock')
@@ -27,6 +30,22 @@ export default class TradeController implements interfaces.Controller {
 		let reqTrade: ITradeDto = req.body;
 		
 		return await this.buyStocksUseCase.invoke(reqTrade)
+			.then((tradeDto: ITradeDto) => {
+				res.status(200).json(tradeDto)
+			})
+			.catch((err: Error) => res.status(500).json({err}));
+	}
+
+	@httpPost('/sellstock')
+	public async sellStock(@request() req: express.Request, @response() res: express.Response){
+		
+		if(!req.body.user_id && !req.body.stock_id){
+			return res.status(400).json({error: 'No user id or stock id provided'});
+		}
+
+		let reqTrade: ITradeDto = req.body;
+		
+		return await this.sellStocksUseCase.invoke(reqTrade)
 			.then((tradeDto: ITradeDto) => {
 				res.status(200).json(tradeDto)
 			})
--- backend/src/usecases/Trades/SellStocksUseCase.ts
+++ backend/src/usecases/Trades/SellStocksUseCase.ts
@@ -3,52 +3,84 @@ import ITradeWriteOnlyRepository from '../../application/repositories/ITradeWrit
 import ITradeDto from '../data_tranfer_objects/ITradeDto';
 import ISellStocksUseCase from './ISellStocksUseCase';
 import IUserWriteOnlyRepository from '../../application/repositories/IUserWriteOnlyRepository';
+import IUserReadOnlyRepository from '../../application/repositories/IUserReadOnlyRepository';
+import IStockDto from '../data_tranfer_objects/IStockDto';
+import IUserDto from '../data_tranfer_objects/IUserDto';
 
 export default class SellStocksUseCase implements ISellStocksUseCase {
 	stockWriteOnlyRepository: IStockWriteOnlyRepository;
 	tradeWriteOnlyRepository: ITradeWriteOnlyRepository;
 	userWriteOnlyRepository: IUserWriteOnlyRepository;
+	userReadOnlyRepository: IUserReadOnlyRepository;
 
 	/**
 	 *
 	 */
 	constructor(_stockWriteOnlyRepository: IStockWriteOnlyRepository, 
 				_tradeWriteOnlyRepository: ITradeWriteOnlyRepository,
-				_userWriteOnlyRepository: IUserWriteOnlyRepository) {
+				_userWriteOnlyRepository: IUserWriteOnlyRepository,
+				_userReadOnlyRepository: IUserReadOnlyRepository) {
 		this.stockWriteOnlyRepository = _stockWriteOnlyRepository;
 		this.tradeWriteOnlyRepository = _tradeWriteOnlyRepository;
 		this.userWriteOnlyRepository = _userWriteOnlyRepository;
+		this.userReadOnlyRepository = _userReadOnlyRepository;
 	}
 
 	async invoke(tradeDto: ITradeDto): Promise<ITradeDto> {
-		try {
-			await this.userWriteOnlyRepository.edit(tradeDto.user_id, {}, {})
-		} catch (e) {
-			throw new Error("Error deducting credit: " + e);
-		}
-
-		try {
-			await this.stockWriteOnlyRepository.edit({
+		return new Promise((resolve, reject) => {
+			let stock: IStockDto[];
+			let returnObj: ITradeDto;
+	
+			this.stockWriteOnlyRepository.edit({
 				id: tradeDto.stock_id,
 				volume: Math.abs(tradeDto.stock_amount!),
-				symbol: '',
-				name: ''
 			},{
 				tradeMode: 1
 			})
-		} catch (e) {
-			throw new Error("Error adding volume to stock: " + e);
-		}
+			.then(stock => {
+				let user: IUserDto;
+	
+				this.userReadOnlyRepository.fetch({
+					id: tradeDto.user_id
+				})
+				.then(user => {
+					if(!user.username)
+						reject("Could not get user details");
+	
+					this.userWriteOnlyRepository.edit(user.username!, {
+						credit: tradeDto.stock_amount! * stock[0].value!
+					}, {
+						tradeMode: 1
+					})
+					.then(async user => {
+						let tradeObj = {
+							stock_id: tradeDto.stock_id,
+							user_id: tradeDto.user_id,
+							stock_amount: tradeDto.stock_amount,
+							stock_value: stock[0].value,
+							time_of_trade: new Date()
+						}
 
-		let newTrade : ITradeDto;
-		
-		try {
-			newTrade = await this.tradeWriteOnlyRepository.create(tradeDto);
-		} catch (e) {
-			throw new Error("Error creating trade: " + e)
-		}
-
-		return newTrade;
+						this.tradeWriteOnlyRepository.create(tradeObj)
+						.then(newTrade => {
+							resolve(newTrade);
+						})
+						.catch(err => {
+							reject("Error creating trade: " + err);
+						})
+					})
+					.catch(err => {
+						reject("Error deducting credit: " + err);
+					})
+				})
+				.catch(err => {
+					reject("Cannot find user: " + err);
+				})
+			})
+			.catch(err => {
+				reject("Error deducting volume from stock: " + err);
+			})
+		});
 	}
 	
 }
