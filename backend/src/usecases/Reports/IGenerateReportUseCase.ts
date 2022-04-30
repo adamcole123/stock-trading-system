b580d53b6a63825f987f4bc3e56548e98f406de2
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2022-04-30 14:32:37 +0100

    latest

--- backend/src/usecases/Reports/IGenerateReportUseCase.ts
+++ backend/src/usecases/Reports/IGenerateReportUseCase.ts
@@ -4,5 +4,5 @@ import ReportType from '../entities/ReportType';
 export default interface IGenerateReportUseCase{
 	completeStockValues(user_id: string, ascending: boolean, report_type: string): Promise<IUserDto>
 	selectedCompanyDetails(user_id: string, stock_ids: string[]): Promise<IUserDto>
-	usersHeldShares(user_id: string): Promise<IUserDto>
+	usersHeldShares(user_id: string, ascending: boolean, report_type: string): Promise<IUserDto>
 }
