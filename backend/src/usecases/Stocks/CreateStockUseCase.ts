aedabbec18c3bdb1467e55388d74b3665a2ce254
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2022-05-11 20:49:52 +0100

    Can create new company stock with random data

--- backend/src/usecases/Stocks/CreateStockUseCase.ts
+++ backend/src/usecases/Stocks/CreateStockUseCase.ts
@@ -14,6 +14,13 @@ export default class CreateStockUseCase implements ICreateStockUseCase {
 
 	async invoke(stock: IStockDto): Promise<IStockDto> {
 		let addedStock: IStockDto;
+
+		if(stock.volume === undefined && stock.value === undefined){
+			stock.volume = Math.round(Math.random() * 100000);
+			stock.value = Number.parseFloat((Math.random() * 1000).toFixed(2));
+			stock.open = Number.parseFloat((Math.random() * 1000).toFixed(2));
+			stock.close = Number.parseFloat((Math.random() * 1000).toFixed(2));
+		}
 		
 		try {
 			addedStock = await this.stockWriteOnlyRepository.create(stock);	
