1e56cfc7e48ff32f88cbf12b263b39b381de1653
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2022-05-29 23:58:09 +0100

    Change complete

--- backend/src/application/repositories/ITradeReadOnlyRepository.ts
+++ backend/src/application/repositories/ITradeReadOnlyRepository.ts
@@ -1,6 +1,7 @@
+import TradeReadOptions from "../../infrastructure/Trade/TradeReadOptions"
 import ITradeDto from "../../usecases/data_tranfer_objects/ITradeDto"
 
 export default interface ITradeReadOnlyRepository {
 	fetchAll(): Promise<ITradeDto[]>
-	fetch(tradeDto: ITradeDto, populateStocks?: boolean): Promise<ITradeDto[]>
+	fetch(tradeDto: ITradeDto, populateStocks?: boolean, options?: TradeReadOptions): Promise<ITradeDto[]>
 }
