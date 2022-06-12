cda5d709fd3f211506f0ee5dd841ac7c35bbd89d
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2022-06-12 13:04:15 +0100

    Can now send emails using gmail server

--- backend/src/configuration/EmailServiceLocator.ts
+++ backend/src/configuration/EmailServiceLocator.ts
@@ -1,10 +1,11 @@
 import { injectable, inject } from "inversify";
+import SendRealEmailUseCase from "../usecases/Email/SendRealEmailUseCase";
 import ISendEmailUseCase from '../usecases/Email/ISendEmailUseCase';
 import SendEmailUseCase from "../usecases/Email/SendEmailUseCase";
 
 @injectable()
 export default class EmailServiceLocator {
 	public GetSendEmailUseCase(): ISendEmailUseCase {
-		return new SendEmailUseCase();
+		return new SendRealEmailUseCase();
 	}
 }
