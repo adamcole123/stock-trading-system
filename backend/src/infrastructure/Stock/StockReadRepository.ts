6bdcfda60cb2b71c8236cc43e8a5b54a62718c46
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2022-10-02 12:47:16 +0100

    PC changes

--- backend/src/infrastructure/Stock/StockReadRepository.ts
+++ backend/src/infrastructure/Stock/StockReadRepository.ts
@@ -17,21 +17,12 @@ export default class StockReadRepository implements IStockReadOnlyRepository {
 	}
 	fetchAll(): Promise<IStockDto[]> {
 		return new Promise(async (resolve, reject) => {
-			let returnData: StockType[] = [];
+			let returnData: IStockDto[] = [];
 			await Stock.find({})
-				.then(res => {
+				.then(res => {+
+
 					res.forEach(stock => {
-						returnData.push(new StockType(
-							stock._id.toString(),
-							stock.symbol,
-							stock.name,
-							stock.value,
-							stock.volume,
-							stock.open,
-							stock.close,
-							stock.latest_trade,
-							stock.gains,
-						))
+						returnData.push(this.transformMongoose(stock))
 					});
 				})
 				.catch(error => {
@@ -42,170 +33,246 @@ export default class StockReadRepository implements IStockReadOnlyRepository {
 		})
 	}
 
-	fetch(stockDto?: IStockDto | IStockDto[], options: StockOptions = {}): Promise<IStockDto[]> {
-		return new Promise(async (resolve, reject) => {
-			let returnData: StockType[] = [];
+	// fetch(stockDto?: IStockDto | IStockDto[], options: StockOptions = {}): Promise<IStockDto[]> {
+	// 	return new Promise(async (resolve, reject) => {
+	// 		let returnData: IStockDto[] = [];
 
-			let sort: { [key: string]: SortOrder | { $meta: "textScore"; }; } = {};
+	// 		let sort: { [key: string]: SortOrder | { $meta: "textScore"; }; } = {};
 
-			if(options.order !== undefined && 
-				options.order.orderBy !== undefined && 
-				options.order.orderDirection !== undefined){
-				sort[options!.order!.orderBy!] = options?.order?.orderDirection === 0 ? -1 : 1;
-			}
+	// 		if(options.order !== undefined && 
+	// 			options.order.orderBy !== undefined && 
+	// 			options.order.orderDirection !== undefined){
+	// 			sort[options!.order!.orderBy!] = options?.order?.orderDirection === 0 ? -1 : 1;
+	// 		}
 
-			if (Array.isArray(stockDto)) {
-				let ids = stockDto.map(stock => stock.id);
-				let stocks = await Stock.find({ _id: { $in: ids } })
-					.sort(sort)
-					.limit(options?.limit!)
-					.exec();
+	// 		if (Array.isArray(stockDto)) {
+	// 			let ids = stockDto.map(stock => stock.id);
+	// 			let stocks = await Stock.find({ _id: { $in: ids } })
+	// 				.sort(sort)
+	// 				.limit(options?.limit!)
+	// 				.exec();
 
-				returnData = stocks.map(stock => {
-					return new StockType(
-						stock!._id.toString(),
-						stock!.symbol,
-						stock!.name,
-						stock!.value,
-						stock!.volume,
-						stock!.open,
-						stock!.close,
-						stock!.latest_trade,
-						stock!.gains,
-					)
-				})
-			} else {
-				if (options.lowestValue) {
-					let returnStock: any = await Stock.find().sort({ value: 1 }).limit(1);
-					returnStock = returnStock[0];
-					resolve([
-						new StockType(
-							returnStock._id.toString(),
-							returnStock.symbol,
-							returnStock.name,
-							returnStock.value,
-							returnStock.volume,
-							returnStock.open,
-							returnStock.close,
-							returnStock.latest_trade,
-							returnStock.gains
-						)
-					]
-					)
-				}
-				let query: StockFetchQuery = stockDto!;
+	// 			returnData = stocks.map(stock => {
+	// 				return this.transformMongoose(stock);
+	// 			})
+	// 		} else {
+	// 			if (options.lowestValue) {
+	// 				let returnStock: any = await Stock.find().sort({ value: 1 }).limit(1);
+	// 				returnStock = returnStock[0];
+	// 				resolve([this.transformMongoose(returnStock)])
+	// 			}
+	// 			let query: StockFetchQuery = stockDto!;
 				
 
-				switch (options?.gainsMode) {
-					case 0:
-						query.gains = { $lt: stockDto?.gains };
-						break;
-					case 1:
-						query.gains = stockDto?.gains;
-						break;
+	// 			switch (options?.gainsMode) {
+	// 				case 0:
+	// 					query.gains = { $lt: stockDto?.gains };
+	// 					break;
+	// 				case 1:
+	// 					query.gains = stockDto?.gains;
+	// 					break;
+	// 				case 2:
+	// 					query.gains = { $gt: stockDto?.gains };
+	// 					break;
+	// 			}
+
+	// 			switch (options?.valueMode) {
+	// 				case 0:
+	// 					query.value = { $lt: stockDto?.value };
+	// 					break;
+	// 				case 1:
+	// 					query.value = stockDto?.value;
+	// 					break;
+	// 				case 2:
+	// 					query.value = { $gt: stockDto?.value };
+	// 					break;
+	// 			}
+
+	// 			switch (options?.volumeMode) {
+	// 				case 0:
+	// 					query.volume = { $lt: stockDto?.volume };
+	// 					break;
+	// 				case 1:
+	// 					query.volume = stockDto?.volume;
+	// 					break;
+	// 				case 2:
+	// 					query.volume = { $gt: stockDto?.volume };
+	// 					break;
+	// 			}
+
+	// 			if (!options?.limit) {
+	// 				options!.limit = await Stock.count({});
+	// 			}
+
+				
+
+	// 			if (options?.page) {
+	// 				try {
+	// 					let returnStocks = await Stock
+	// 						.find({ ...query })
+	// 						.sort(sort)
+	// 						.skip((options?.page * options.limit!) - (options.limit!))
+	// 						.limit(options.limit!);
+	// 					returnStocks.forEach(stock => {
+	// 						returnData.push(this.transformMongoose(stock));
+	// 					})
+	// 				} catch (error) {
+	// 					reject(error);
+	// 				}
+	// 			} else {
+	// 				try {
+	// 					if (stockDto?.id) {
+	// 						let stock = await Stock.findOne({ _id: stockDto.id }).exec();
+	// 						returnData.push(this.transformMongoose(stock))
+	// 					} else {
+	// 						let stocks = await Stock.find({ symbol: query.symbol })
+	// 							.sort(sort)
+	// 							.limit(options?.limit!)
+	// 							.exec();
+	// 						returnData = stocks.map(stock => {
+	// 							return this.transformMongoose(stock);
+	// 						});
+	// 					}
+	// 				} catch (e) {
+	// 					reject(e);
+	// 				}
+	// 			}
+
+	// 		}
+	// 		resolve(returnData!);
+	// 	})
+	// }
+
+	
+	fetch(stockDto?: IStockDto | IStockDto[], options: StockOptions = {}): Promise<IStockDto[]> {
+		return new Promise(async (resolve, reject) => {
+			try { 
+				let sort: { [key: string]: SortOrder | { $meta: "textScore"; }; } = {};
+				
+				let result: IStockDto[];
+				
+				if(options.order !== undefined && 
+					options.order.orderBy !== undefined && 
+					options.order.orderDirection !== undefined){
+					sort[options!.order!.orderBy!] = options?.order?.orderDirection === 0 ? -1 : 1;
+				}
+				
+				if(Array.isArray(stockDto)){
+					let ids = stockDto.map(stock => stock.id);
+					let stocks = await Stock.find({ _id: { $in: ids } })
+						.sort(sort)
+						.limit(options?.limit!)
+						.exec();
+						
+						result = stocks.map(stock => {
+							return this.transformMongoose(stock);
+						})
+					} else {
+					let query = this.buildQuery(stockDto!, options);
+					
+					if (!options?.limit) {
+						options!.limit = await Stock.count({});
+					}
+	
+					result = await this.queryDB(options, query, sort, stockDto);
+	
+					return resolve(result);
+				}
+			} catch(e) {
+				return reject(e);
+			}
+		})
+	}
+	
+	private async queryDB(
+		options: StockOptions, query: IStockDto | StockFetchQuery, 
+		sort: { [key: string]: SortOrder | { $meta: "textScore"; }; }, 
+		stockDto: IStockDto | undefined
+		): Promise<IStockDto[]> {
+		let queryResult: IStockDto[] = [];
+		if (options?.page) {
+			try {
+				let returnStocks = await Stock
+				.find({ ...query })
+				.sort(sort)
+					.skip((options?.page * options.limit!) - (options.limit!))
+					.limit(options.limit!);
+					returnStocks.forEach(stock => {
+						queryResult.push(this.transformMongoose(stock));
+					});
+			} catch (error: any) {
+				throw new Error(error);
+			}
+		} else {
+			try {
+				if (stockDto?.id) {
+					let stock = await Stock.findOne({ _id: stockDto.id }).exec();
+					queryResult.push(this.transformMongoose(stock));
+				} else {
+					let stocks = await Stock.find({ symbol: query.symbol })
+					.sort(sort)
+					.limit(options?.limit!)
+						.exec();
+						queryResult = stocks.map(stock => {
+							return this.transformMongoose(stock);
+					});
+				}
+			} catch (error: any) {
+				throw new Error(error);
+			}
+		}
+		
+		return queryResult;
+	}
+	
+	private buildQuery(stockDto: IStockDto, options: StockOptions = {}): StockFetchQuery {
+		let query: StockFetchQuery = stockDto;
+		
+		switch (options?.gainsMode) {
+			case 0:
+				query.gains = { $lt: stockDto?.gains };
+				break;
+				case 1:
+					query.gains = stockDto?.gains;
+					break;
 					case 2:
 						query.gains = { $gt: stockDto?.gains };
 						break;
-				}
+					}
 
-				switch (options?.valueMode) {
-					case 0:
-						query.value = { $lt: stockDto?.value };
-						break;
-					case 1:
-						query.value = stockDto?.value;
-						break;
-					case 2:
-						query.value = { $gt: stockDto?.value };
-						break;
+		switch (options?.valueMode) {
+			case 0:
+				query.value = { $lt: stockDto?.value };
+				break;
+			case 1:
+				query.value = stockDto?.value;
+				break;
+				case 2:
+					query.value = { $gt: stockDto?.value };
+					break;
 				}
-
+				
 				switch (options?.volumeMode) {
 					case 0:
 						query.volume = { $lt: stockDto?.volume };
-						break;
-					case 1:
-						query.volume = stockDto?.volume;
-						break;
-					case 2:
-						query.volume = { $gt: stockDto?.volume };
-						break;
-				}
-
-				if (!options?.limit) {
-					options!.limit = await Stock.count({});
-				}
-
-				
-
-				if (options?.page) {
-					try {
-						let returnStocks = await Stock
-							.find({ ...query })
-							.sort(sort)
-							.skip((options?.page * options.limit!) - (options.limit!))
-							.limit(options.limit!);
-						returnStocks.forEach(stock => {
-							returnData.push(new StockType(
-								stock._id.toString(),
-								stock.symbol,
-								stock.name,
-								stock.value,
-								stock.volume,
-								stock.open,
-								stock.close,
-								stock.latest_trade,
-								stock.gains,
-							))
-						})
-					} catch (error) {
-						reject(error);
-					}
-				} else {
-					try {
-						if (stockDto?.id) {
-							let stock = await Stock.findOne({ _id: stockDto.id }).exec();
-							returnData.push(new StockType(
-								stock!._id.toString(),
-								stock!.symbol,
-								stock!.name,
-								stock!.value,
-								stock!.volume,
-								stock!.open,
-								stock!.close,
-								stock!.latest_trade,
-								stock!.gains
-							))
-						} else {
-							let stocks = await Stock.find({ symbol: query.symbol })
-								.sort(sort)
-								.limit(options?.limit!)
-								.exec();
-							returnData = stocks.map(stock => {
-								return new StockType(
-									stock!._id.toString(),
-									stock!.symbol,
-									stock!.name,
-									stock!.value,
-									stock!.volume,
-									stock!.open,
-									stock!.close,
-									stock!.latest_trade,
-									stock!.gains,
-								)
-							})
-						}
-					} catch (e) {
-						reject(e);
-					}
-				}
-
-			}
-			resolve(returnData!);
-		})
+				break;
+			case 1:
+				query.volume = stockDto?.volume;
+				break;
+				case 2:
+				query.volume = { $gt: stockDto?.volume };
+				break;
+		}
+		
+		return query;
 	}
-
+	
 	count() {
 		return Stock.count();
+	}
+	
+	private transformMongoose(doc: any): IStockDto {
+		const { _id, ...rest } = doc;
+		return { id: _id.toString(), ...rest };
 	}
 }
