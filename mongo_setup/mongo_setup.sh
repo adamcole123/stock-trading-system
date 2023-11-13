9ad343402b45d5988e785b68d5c24d1414ccffd4
Adam Cole <32772788+adamcole123@users.noreply.github.com>
2023-11-13 19:20:12 +0000

    Made some initial changes

--- mongo_setup/mongo_setup.sh
+++ mongo_setup/mongo_setup.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-echo "sleeping for 10 seconds"
-sleep 10
+echo "Checking status of MongoDB instances..."
 
-echo mongo_setup.sh time now: `date +"%T" `
+for host in mongo1 mongo2 mongo3
+do
+  echo "Checking $host..."
+  until mongo --host $host:27017 --eval "print(\"waited for connection\")"
+  do
+    sleep 5
+  done
+done
+
+echo "All MongoDB instances are ready. Initiating replica set..."
+
 mongo --host mongo1:27017 <<EOF
   var cfg = {
     "_id": "rs0",
